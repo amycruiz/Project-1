fb2a49b6607ef1b0c4a6c2a31e0c07a46cf0e6cf
amycruiz <acr@njit.edu>
2024-10-27 23:39:08 -0400

    added command and commandhandler classes for command registration and execution.

---
+++ app/commands/command_handler.py
@@ -0,0 +1,30 @@
+'''Registering and executing commands file'''
+
+import logging
+from abc import ABC, abstractmethod
+
+class Command(ABC):
+    @abstractmethod
+    def execute(self, *args):
+        pass
+
+class CommandHandler:
+    def __init__(self):
+        self.commands = {}
+    
+    def registerCommand(self, commandName: str, command: Command):
+        '''Register a new command to the handler.'''
+        self.commands[commandName] = command
+        logging.info(f"Registered command: {commandName}")
+    
+    def executeCommand(self, commandName: str, *args):
+        '''Execute a command by name, with optional arguments.'''
+        try:
+            logging.info(f"Executing command: {commandName} with arguments: {args}")
+            self.commands[commandName].execute(*args)
+        except KeyError: 
+            logging.error(f"Command '{commandName}' not found.")
+            return f"Command '{commandName}' not found."
+        except Exception as e:
+            logging.error(f"Error executing command '{commandName}': {e}")
+            return f"Error executing command '{commandName}': {e}"

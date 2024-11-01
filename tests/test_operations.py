917135989c91182216de60e418eff3a50a1d7442
amycruiz <acr@njit.edu>
2024-11-01 10:10:28 -0400

    added tests for menu plugin in tests folder.

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,17 +1,18 @@
+import pytest
 from app.calculator.operations import add, subtract, multiply, divide
 
 def addition_test():
-    assert add(3,4) == 7
-    assert add(-5,6) == 1
+    assert add(3,4) == 7.0
+    assert add(-5,6) == 1.0
 
 def subtraction_test():
-    assert subtract(5,4) == 1
-    assert subtract(4,8) == -4
+    assert subtract(5,4) == 1.0
+    assert subtract(4,8) == -4.0
 
 def multiplication_test():
-    assert multiply(3,5) == 15
-    assert multiply(-2, 4) == -8
+    assert multiply(3,5) == 15.0
+    assert multiply(-2, 4) == -8.0
 
 def division_test():
-    assert divide(10,5) == 2
-    assert divide(-15,3) == -5
+    assert divide(10,5) == 2.0
+    assert divide(-15,3) == -5.0
---
+++ tests/test_plugin_menu.py
@@ -0,0 +1,21 @@
+import pytest
+from app.plugins.menu import MenuCommmand
+
+def menu_command_test ():
+    return MenuCommmand()
+
+def returns_expected_menu_test(menu_command):
+    expectedOutput = (
+        "Available commands:\n"
+        "add [num1] [num2] - Add two numbers\n"
+        "subtract [num1] [num2] - Subtract two numbers\n"
+        "multiply [num1] [num2] - Multiply two numbers\n"
+        "divide [num1] [num2] - Divide two numbers\n"
+        "savehistory [expression] [num1] [num2] [result] - Save a specific calculation to history\n"
+        "loadhistory - Load saved calculation history\n"
+        "clearhistory - Clear all saved calculation history\n"
+        "undohistory - Undo your last saved calculation history entry\n"
+        "menu - Show a menu of all available commands\n"
+        "exit - Exit the calculator"
+    )
+    assert menu_command.execute() == expectedOutput
